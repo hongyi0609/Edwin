5d932a65f43a4a98569cb45ad383a75f862f4463
hongyi0609 <hongyi0609@126.com>
2017-10-06 00:47:34 +0800

    OTA0_1_function_gradle, 1.自定义gradle插件 2.发布到本地maven仓库
    Signed-off-by: hongyi0609 <hongyi0609@126.com>

---
+++ gradleplugin/src/main/groovy/com/bignerdranch/android/gradleplugin/GradleImpl.groovy
@@ -0,0 +1,1 @@
+package com.bignerdranch.android.gradlepluginimport org.gradle.api.Pluginimport org.gradle.api.Projectclass GradleImpl implements Plugin<Project>{ @Override void apply(Project project) {  System.out.println("========================")  System.out.println("hello gradle plugin!")  System.out.println("========================") }}
---
+++ gradleplugin/src/main/groovy/com/bignerdranch/android/gradleplugin/GradlePluginImpl.groovy
@@ -0,0 +1,16 @@
+package com.bignerdranch.android.gradleplugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class GradlePluginImpl implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        System.out.println("========================")
+        System.out.println("hello gradle plugin!")
+        System.out.println("========================")
+        project.task('testTask') << {
+            println("Hello, test Task with plugin!")
+        }
+    }
+}
